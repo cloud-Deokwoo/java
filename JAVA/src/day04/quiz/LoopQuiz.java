56b20d2f1c404c8b0da8ec15edcc95f553fed819
sc_tjoeun <sc_tjoeun@TJOEUN-01>
2023-08-23 11:16:37 +0900

    두번째 git commit

--- JAVA/src/day04/quiz/LoopQuiz.java
+++ JAVA/src/day04/quiz/LoopQuiz.java
@@ -1,5 +1,7 @@
 package day04.quiz;
 
+import java.util.Scanner;
+
 public class LoopQuiz {
 
 	public static void main(String[] args) {
@@ -27,13 +29,45 @@ public class LoopQuiz {
 		//   자바의 Scanner 객체를 이용하여 콘솔에 데이터를 입력
 		//   입력된 데이터를 for문을 사용하여 거꾸로(reverse) 출력하세요
 		//   (String.charAt(index)를 사용)
-		
+		System.out.println("\n======= 입력 값 거꾸로 출력하기 =======");
+		Scanner scan = new Scanner(System.in);
+		String str = scan.nextLine();   // hello
+		String reverse = "";
+		for (int i=str.length(); i>0; i--) {
+			reverse +=str.charAt(i-1);
+			System.out.print(str.charAt(i-1));
+		}
+		System.out.println("거꾸로 문자열 : "+reverse);
+		scan.close();
 		
 		
 		// 3. 로또 번호 추출하기
 		//   앞서 배운 난수를 활용하여, 중복되는 번호 없이 1 ~ 45까지의 숫자 중
 		//   6개의 숫자를 랜덤추출하고, 콘솔에 출력하세요. 
 		//   (while문에 for)
+		System.out.println("======= 로또 추출 =======");
+		int[] lotto = {0,0,0,0,0,0};
+		
+		int index = 0;
+		while(true) {
+			// 랜덤값 생성
+			int rand = (int)(Math.random() * 45) + 1;  //1 ~ 45
+			int i = 0;  // index와 비교하기 위해서
+			for (i=0; i<index;i++) {
+				if(rand == lotto[i]) {  //추첨된 번호가 중복됨
+					break;
+				}
+			}
+			if(index == i) {  // 추첨된 번호에 있지 않음
+				lotto[index++] = rand;
+			}
+			if(index > 5) break; // 6개 번호 추첨 끝
+		}
+		
+		//결과 출력
+		for(int i=0; i<lotto.length; i++) {
+			System.out.print(lotto[i] + "\t");
+		}
 
 	}
 
