aa7da0295d14e497307896e8ddbde9c2134a99da
sc_tjoeun <sc_tjoeun@host.docker.internal>
2023-09-27 18:19:12 +0900

    9월 27일 수업

---
+++ JAVA/src/day30/Test.java
@@ -0,0 +1,14 @@
+package day30;
+
+import java.util.HashMap;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String, Integer> test = new HashMap<String, Integer>();
+		test.put("test", 0);
+		
+	}
+
+}
